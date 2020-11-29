b0e5403678dafa901f8240e5ee319c4a990c5134
Jazer kemuel  Zayas <35165200+jkpz10@users.noreply.github.com>
2020-11-29 15:54:02 +0800

    build coming soon page (#1)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import { createApp } from 'vue'
+// App imports
 import App from './App.vue'
+import router from './router/router'
+// CSS frameworks imports
 import './tailwind.css'
 
 
 // import './assets/css/main.css'
 // import "tailwindcss/tailwind.css"
 
-createApp(App).mount('#app')
+// createApp(App).use(router).mount('#app')
+
+const app = createApp(App);
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router)
+
+app.mount('#app')
